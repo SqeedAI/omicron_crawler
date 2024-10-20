4a4d93c09ef74dcd48d2ae92e09be81188798ed1
erik9631 <erik9631@gmail.com>
2024-10-20 12:25:09 +0200

    feat: Added chrome driver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use thirtyfour::{DesiredCapabilities, WebDriver};
+
+async fn launch() {
+    let capabilities = DesiredCapabilities::chrome();
+    let driver = WebDriver::new("http://localhost:9515", capabilities).await.expect("Failed to create driver");
+    driver.goto("https://www.google.com/").await.expect("Failed to go to google");
 }
+
+
+#[tokio::main]
+async fn main() {
+    launch().await;
+}
+
+
